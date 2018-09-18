0f309d7224f0755527ba8148345ce8f727b565b0
muppidathic <43374195+muppidathic@users.noreply.github.com>
2018-09-18 17:34:32 +0530

    Create vowle or constant.py

---
+++ vowle or constant.py
@@ -0,0 +1,8 @@
+1=input("Input a letter of the alphabet:")
+if/in('a;'e;'i;'o;'u;):
+print("%s is a vowel"%1)
+elif 1=='y':
+  print("sometime letter y stand for vowel,sometimes stand for constant.")  
+else
+print("%s is a constant."%1)
+
