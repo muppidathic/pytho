6655e7c12f1eadd8e935cbb24749509f084c5a86
muppidathic <43374195+muppidathic@users.noreply.github.com>
2018-09-25 16:52:57 +0530

    Create find year is leap year or not.py

---
+++ find year is leap year or not.py
@@ -0,0 +1,13 @@
+
+x=int(input("a="))
+if(x%4==0):
+    print(" year is leap year")
+else:
+    print(" year is not leap year")
+    
+x=int(input("a="))
+if(x%4==0):
+    print(" year is leap year")
+else:
+    print(" year is not leap year")
+    
